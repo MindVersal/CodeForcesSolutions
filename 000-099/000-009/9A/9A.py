5584366e333ce1453c844cbfba749c41549842f4
MindVersal <antajchik@gmail.com>
2018-06-12 10:42:26 +0300

    10:44 12.06.2018

--- 000-099/000-009/9A/9A.py
+++ 000-099/000-009/9A/9A.py
@@ -3,5 +3,20 @@ Program for solution problem 9A
 
 """
 
+
+def calculate_probability():
+    input_numbers = [x for x in input()]
+    big_number = input_numbers[0] if input_numbers[0] >= input_numbers[2] else input_numbers[2]
+    probability = {
+        '1': '1/1',
+        '2': '5/6',
+        '3': '2/3',
+        '4': '1/2',
+        '5': '1/3',
+        '6': '1/6'
+    }
+    return probability[big_number]
+
+
 if __name__ == '__main__':
-    print('Problem 9A')
+    print(calculate_probability())
