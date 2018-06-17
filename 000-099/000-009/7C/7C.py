e88ca1bc6fe1cf12fc0532bda2ddaeed7962684e
MindVersal <antajchik@gmail.com>
2018-06-17 13:59:08 +0300

    14:00 17.06.2018

--- 000-099/000-009/7C/7C.py
+++ 000-099/000-009/7C/7C.py
@@ -2,7 +2,20 @@
 Program for solving problem 7C
 
 """
+import math
+
+
+def find_point_belongs_to_line(numbers):
+    limit_number = 5 * 100
+    answer = '-1'
+    koef_a = int(numbers[0])
+    koef_b = int(numbers[1])
+    koef_c = int(numbers[2])
+    d = abs(koef_c) / math.sqrt(math.pow(koef_a, 2) + math.pow(koef_b, 2))
+    answer = str(d)
+    return answer
 
 
 if __name__ == '__main__':
-    print('7C')
+    input_numbers = [x for x in input() if x != ' ']
+    print(find_point_belongs_to_line(input_numbers))
